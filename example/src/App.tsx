2e62c62cf6e7c63faf03263346de9df953063fe3
莫应涛 <limoer@moyingtaodeAir.lan>
2022-11-21 16:58:32 +0800

    feat: 更新demo

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,29 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import { multiply, showActivity } from 'react-native-geyan';
+const logo = require('./assets/logo.png');
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
-
+  const [token, setToken] = React.useState('');
   React.useEffect(() => {
     multiply(3, 7).then(setResult);
   }, []);
-
+  const resolved = Image.resolveAssetSource(logo);
+  async function handleOpenActivity() {
+    try {
+      const currentToken = await showActivity({ logo: resolved.uri });
+      setToken(currentToken);
+    } catch (error) {}
+  }
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
-      <TouchableOpacity onPress={() => showActivity()}>
+      <TouchableOpacity onPress={handleOpenActivity}>
         <Text>Open Activity</Text>
       </TouchableOpacity>
+      <Text>Token is: {token}</Text>
     </View>
   );
 }
